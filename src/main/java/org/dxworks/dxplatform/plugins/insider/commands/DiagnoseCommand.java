ae1a3b6c0f0e8a68a29734e5c094ff322d39aa14
Mario Rivis <mario.rivis@gmail.com>
2019-05-21 11:38:12 +0300

    Fixed results with positive value and refactored commands

--- src/main/java/org/dxworks/dxplatform/plugins/insider/commands/DiagnoseCommand.java
+++ src/main/java/org/dxworks/dxplatform/plugins/insider/commands/DiagnoseCommand.java
@@ -1,9 +1,16 @@
 package org.dxworks.dxplatform.plugins.insider.commands;
 
 import org.dxworks.dxplatform.plugins.insider.InsiderFile;
-import org.dxworks.dxplatform.plugins.insider.utils.transformers.JsonToCsvTransformer;
+import org.dxworks.dxplatform.plugins.insider.technology.finder.model.Technology;
+import org.dxworks.dxplatform.plugins.insider.technology.finder.parsers.JsonFingerprintParser;
+import org.dxworks.dxplatform.plugins.insider.utils.ImportUtils;
+import org.dxworks.dxplatform.plugins.insider.utils.LibraryImport;
+import org.dxworks.dxplatform.plugins.insider.utils.LibraryImportInOtherTechnology;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class DiagnoseCommand implements NoFilesCommand {
 
@@ -21,9 +28,39 @@ public class DiagnoseCommand implements NoFilesCommand {
 
     @Override
     public void execute(List<InsiderFile> insiderFiles, String[] args) {
-        JsonToCsvTransformer jsonToCsvTransformer = new JsonToCsvTransformer();
 
-        jsonToCsvTransformer.diagnoseJsonFile(file);
+        JsonFingerprintParser jsonFingerprintParser = new JsonFingerprintParser();
+        List<Technology> jsonTechnologies = jsonFingerprintParser.parseTechnologiesFile(file);
+
+        List<String> duplicatedTechnologies = jsonTechnologies.stream()
+                .collect(Collectors.groupingBy(Technology::getName, Collectors.counting()))
+                .entrySet().stream()
+                .filter(e -> e.getValue() > 1)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+
+        System.out.println("Duplicated technologies: " + duplicatedTechnologies.toString());
+
+        List<LibraryImportInOtherTechnology> duplicatedFingerPrints = new ArrayList<>();
+
+        jsonTechnologies.forEach(technology -> {
+            List<String> fingerprints = technology.getFingerprints();
+            fingerprints.forEach(fingerprint -> {
+                jsonTechnologies.forEach(otherTechnology -> {
+                    if (technology.equals(otherTechnology))
+                        return;
+                    if (otherTechnology.getFingerprints().contains(fingerprint)) {
+                        duplicatedFingerPrints.add(new LibraryImportInOtherTechnology(fingerprint, technology.getName(), otherTechnology.getName()));
+                    }
+                });
+            });
+        });
+
+        System.out.println("Duplicated Fingerprints: " + duplicatedFingerPrints.stream()
+                .distinct()
+                .map(LibraryImport::get_import)
+                .map(ImportUtils::unwrapImportPackage)
+                .collect(Collectors.toList()));
     }
 
     @Override
--- src/main/java/org/dxworks/dxplatform/plugins/insider/commands/HelpCommand.java
+++ src/main/java/org/dxworks/dxplatform/plugins/insider/commands/HelpCommand.java
@@ -17,14 +17,21 @@ public class HelpCommand implements NoFilesCommand {
 
     @Override
     public void execute(List<InsiderFile> insiderFiles, String[] args) {
-        String usage = "Insider 1.0  -  usage guide:\n\n";
+        String usage = "Insider 1.0  -  usage guide:\n";
+        usage += "Configure the source root and the project id in the config/insider-conf.properties file\n\n";
+
+        usage += "This is a list of the commands:\n";
+
         usage += Stream.of(new HelpCommand(),
                 new VersionCommand(),
                 new AddCommand(),
                 new ConvertCommand(),
                 new DiagnoseCommand(),
                 new DetectCommand(),
-                new FindCommand()).map(InsiderCommand::usage).collect(Collectors.joining("\n"));
+                new FindCommand())
+                .map(InsiderCommand::usage)
+                .map(s -> "\t" + s)
+                .collect(Collectors.joining("\n"));
 
         usage += "\n\nPlease run insider with the specified commands from the folder you have installed Insider to!\n";
 
